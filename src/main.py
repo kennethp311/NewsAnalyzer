b2c41be61da491baf310ebe9be6b423b8935e982
Kenneth Park <kennethpark311@gmail.com>
2024-11-04 17:05:06 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from NewsAnalyzer import NewsAnalyzer
 from FetchNews import FetchNews
+from FetchStock import FetchStock
 from Config import db_config, api_keys
 from datetime import datetime, timedelta
 
@@ -7,14 +8,18 @@ from datetime import datetime, timedelta
 def Fetch_News(table_name, news_search_topic):
     today = datetime.today()
     dates_30_days_prior = [(today - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(30)]    
-    
     FetchNews_obj = FetchNews(db_config, api_keys['News API'], table_name)
 
     for day in dates_30_days_prior:
         articles = FetchNews_obj.fetch_news_at_date(news_search_topic, day)
         FetchNews_obj.store_articles_in_mysql(articles)
+        #FetchNews_obj.Cleanup_table()
 
-    #FetchNews_obj.Cleanup_table()
+
+
+def Fetch_Stocks(ticker_symbol, period):
+    FetchStock_obj = FetchStock(db_config, ticker_symbol)
+    FetchStock_obj.FetchStock_DB(period)
 
 
 def AnalyzeNews(table_name, stock_name):
@@ -31,14 +36,14 @@ def PlotNews(table_name, stock_name):
 
 
 def main():
-    # stock_name = "AMD"  # Example stock name
-    stock_name = input("Enter Stock Ticker Symbol: ")
-    table_name = f'{stock_name.lower()}_article_data'
-    news_search_topic = f'{stock_name} stock performance'
+    ticker_symbol = input("Enter Stock Ticker Symbol: ")
+    table_name = f'{ticker_symbol.lower()}_article_data'
+    news_search_topic = f'{ticker_symbol} stock performance'
 
     # Fetch_News(table_name, news_search_topic)
-    # AnalyzeNews(table_name, stock_name)
-    PlotNews(table_name, stock_name)    
+    # AnalyzeNews(table_name, ticker_symbol)
+    # PlotNews(table_name, ticker_symbol)    
+    # Fetch_Stocks(ticker_symbol, '1mo')
 
 
 
