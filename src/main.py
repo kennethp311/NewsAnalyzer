6eae9bcf76e0b68b364af796520433044fc64567
Kenneth Park <kennethpark311@gmail.com>
2024-11-05 20:10:49 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from FetchStock import FetchStock
 from Config import db_config, api_keys
 
 
-def Fetch_News(table_name, news_search_topic):
-    FetchNews_obj = FetchNews(db_config, api_keys['News API'], table_name)    
-    FetchNews_obj.FetchNews_DB(news_search_topic)
+def Fetch_News(ticker_symbol):
+    FetchNews_obj = FetchNews(db_config, api_keys['News API'], ticker_symbol)    
+    FetchNews_obj.FetchNews_DB()
 
 def Fetch_Stocks(ticker_symbol, period):
     FetchStock_obj = FetchStock(db_config, ticker_symbol)
@@ -16,49 +16,52 @@ def Plot_Stocks(ticker_symbol):
     FetchStock_obj = FetchStock(db_config, ticker_symbol)
     FetchStock_obj.plot_close_prices()
 
-def AnalyzeNews(table_name, stock_name):
-    NewsAnalyzer_obj = NewsAnalyzer(db_config, api_keys['Openai API'], table_name, stock_name)
+def AnalyzeNews(ticker_symbol):
+    NewsAnalyzer_obj = NewsAnalyzer(db_config, api_keys['Openai API'], ticker_symbol)
     NewsAnalyzer_obj.analyze_articles_relevancy()
     NewsAnalyzer_obj.analyze_articles_opinion()
 
-
-def PlotNews(table_name, stock_name):
-    NewsAnalyzer_obj = NewsAnalyzer(db_config, api_keys['Openai API'], table_name, stock_name)
+def PlotNews(ticker_symbol):
+    NewsAnalyzer_obj = NewsAnalyzer(db_config, api_keys['Openai API'], ticker_symbol)
     NewsAnalyzer_obj.PlotResult(NewsAnalyzer_obj.get_results_of_occurences())
 
+
+def Final_PlotScore(ticker_symbol):
+    NewsAnalyzer_obj = NewsAnalyzer(db_config, api_keys['Openai API'], ticker_symbol)
+
+    if NewsAnalyzer_obj.table_exists(NewsAnalyzer_obj.article_table) == False:
+        decision1 = input(f"{NewsAnalyzer_obj.article_table} doesn't exist, so would you like to create one? (Y/N): ")
+        decision2 = input("Would you also like me to analyze and plot the news too? (Y/N): ")
+
+        if decision1 == 'Y':     
+            Fetch_News(ticker_symbol)
+        
+        if decision2 == 'Y': 
+            AnalyzeNews(ticker_symbol)
+            PlotNews(ticker_symbol)
+
+    if NewsAnalyzer_obj.table_exists(NewsAnalyzer_obj.stock_table) == False:
+        print(f"{NewsAnalyzer_obj.stock_table} doesn't exist, hence we are creating one for a 3mo period.")
+        Fetch_Stocks(ticker_symbol, '3mo')
+
+    NewsAnalyzer_obj.plot_news_and_stocks_relationship(NewsAnalyzer_obj.ScoreResult())
 
 
 
 def main():
     ticker_symbol = input("Enter Stock Ticker Symbol: ")
     table_name = f'{ticker_symbol.lower()}_article_data'
-    news_search_topic = f'{ticker_symbol} stock performance'
+    # news_search_topic = f'{ticker_symbol} stock performance'
 
-    # Fetch_News(table_name, news_search_topic)
-    # AnalyzeNews(table_name, ticker_symbol)
-    # PlotNews(table_name, ticker_symbol)    
-    # Fetch_Stocks(ticker_symbol, '1mo')
+    # Fetch_News(ticker_symbol)
+    # AnalyzeNews(ticker_symbol)
+    # PlotNews(ticker_symbol)    
+    # Fetch_Stocks(ticker_symbol, '3mo')
     # Plot_Stocks(ticker_symbol)
+    # Fetch_Stocks(ticker_symbol, '3mo')
+    Final_PlotScore(ticker_symbol)
 
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-# from datetime import datetime, timedelta
-# def Fetch_News(table_name, news_search_topic):
-#     today = datetime.today()
-#     dates_30_days_prior = [(today - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(30)]    
-#     FetchNews_obj = FetchNews(db_config, api_keys['News API'], table_name)
-
-#     for day in dates_30_days_prior:
-#         articles = FetchNews_obj.fetch_news_at_date(news_search_topic, day)
-#         FetchNews_obj.store_articles_in_mysql(articles)
-#         #FetchNews_obj.Cleanup_table()
